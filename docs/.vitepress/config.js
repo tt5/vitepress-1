b4c24105b49d426e594b560254c6bdb501ed8050
n <2585803+tt5@users.noreply.github.com>
2023-03-24 17:46:05 +0100

    content

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -103,9 +103,38 @@ export default defineConfig({
       {
         text: 'progr',
         items: [
-          { text: 'js', link: '/progr/js' },
+          { text: 'progr-bash', link: '/progr/progr-bash' },
+          { text: 'progr-c', link: '/progr/progr-c' },
+          { text: 'progr-cpp', link: '/progr/progr-cpp' },
+          { text: 'progr-gawk', link: '/progr/progr-gawk' },
+          { text: 'progr-git', link: '/progr/progr-git' },
+          { text: 'progr-go', link: '/progr/progr-go' },
+          { text: 'progr-java', link: '/progr/progr-java' },
+          { text: 'progr-js', link: '/progr/progr-js' },
+          { text: 'progr-json', link: '/progr/progr-json' },
+          { text: 'progr-linux-inside', link: '/progr/progr-linux-inside' },
+          { text: 'progr-linux', link: '/progr/progr-linux' },
+          { text: 'progr-lua', link: '/progr/progr-lua' },
+          { text: 'progr-python', link: '/progr/progr-python' },
+          { text: 'progr-regex', link: '/progr/progr-regex' },
+          { text: 'progr-r', link: '/progr/progr-r' },
+          { text: 'progr-rust', link: '/progr/progr-rust' },
+          { text: 'progr-vim', link: '/progr/progr-vim' },
         ]
-      }
+      },
+      {
+        text: 'math',
+        items: [
+          { text: 'math-abstralg', link: '/math/math-abstralg' },
+          { text: 'math-calculus', link: '/math/math-calculus' },
+          { text: 'math-cat', link: '/math/math-cat' },
+          { text: 'math-inf', link: '/math/math-inf' },
+          { text: 'math-intro', link: '/math/math-intro' },
+          { text: 'math-linalg', link: '/math/math-linalg' },
+          { text: 'math-module', link: '/math/math-module' },
+          { text: 'math-presentationth', link: '/math/math-presentationth' },
+        ]
+      },
     ]
   },
   markdown: {
--- docs/index.md
+++ docs/index.md
@@ -1,6 +1,4 @@
 ---
 ---
 
-# KaTeX Demo
-
-$\sqrt{4x-1}+(1+x)^2$
+# Notes
